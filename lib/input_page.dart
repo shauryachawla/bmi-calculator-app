ceb1a68bd32f9538de611a7922bfbd018285101c
shauryachawla <emorres25@gmail.com>
2020-04-27 21:58:04 +0530

    refactored code.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const cardDefaultColor = 0xff1d1e33;
+const bottomContainerColor = 0xffeb1bbb;
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -24,58 +28,55 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
                 Expanded(
-                  child: Container(
-//                    width: 170,
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xff1d1e33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      )),
+                  child: ReusableCard(colour: Color(cardDefaultColor)),
                 ),
                 Expanded(
-                  child: Container(
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xff1d1e33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      )),
+                  child: ReusableCard(colour: Color(cardDefaultColor)),
                 ),
               ],
             ),
           ),
           Expanded(
-            child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xff1d1e33),
-                  borderRadius: BorderRadius.circular(10.0),
-                )),
+            child: ReusableCard(colour: Color(cardDefaultColor)),
           ),
           Expanded(
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
                 Expanded(
-                  child: Container(
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xff1d1e33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      )),
+                  child: ReusableCard(colour: Color(cardDefaultColor)),
                 ),
                 Expanded(
-                  child: Container(
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xff1d1e33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      )),
+                  child: ReusableCard(colour: Color(cardDefaultColor)),
                 ),
               ],
             ),
           ),
+          Container(
+//              child: Center(child: Text('CALCULATE!')),
+              margin: EdgeInsets.only(top: 10),
+              color: Color(bottomContainerColor),
+              width: double.infinity,
+              height: bottomContainerHeight),
         ],
       ),
     );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+//                    width: 170,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10.0),
+        ));
   }
 }
